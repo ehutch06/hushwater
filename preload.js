3e7077e73e9ab84493fbc3f204dd3f08da3de5ae
ehutch06 <ethanhutchison2006@gmail.com>
2026-09-18 23:53:40 -0700

    Add desktop updater bridge

---
+++ preload.js
@@ -0,0 +1,8 @@
+const { contextBridge, ipcRenderer } = require('electron');
+contextBridge.exposeInMainWorld('hushwaterDesktop', {
+  getVersion: () => ipcRenderer.invoke('app-version'),
+  checkForUpdates: () => ipcRenderer.invoke('update-check'),
+  downloadUpdate: () => ipcRenderer.invoke('update-download'),
+  installUpdate: () => ipcRenderer.invoke('update-install'),
+  onUpdateStatus: cb => ipcRenderer.on('update-status', (_event, data) => cb(data))
+});
