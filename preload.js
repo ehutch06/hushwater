5ff697c772327efb4e3bed70ab584f710c0748af
ehutch06 <ethanhutchison2006@gmail.com>
2026-09-26 01:08:34 -0700

    Expose current updater status

--- preload.js
+++ preload.js
@@ -2,6 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('hushwaterDesktop', {
   getVersion: () => ipcRenderer.invoke('app-version'),
   checkForUpdates: () => ipcRenderer.invoke('update-check'),
+  getUpdateStatus: () => ipcRenderer.invoke('update-status-current'),
   downloadUpdate: () => ipcRenderer.invoke('update-download'),
   installUpdate: () => ipcRenderer.invoke('update-install'),
   onUpdateStatus: cb => ipcRenderer.on('update-status', (_event, data) => cb(data))
